426adcd58736a3df8b892c5123d4e5e5c43e0381
Roberto <robertocarlosmedina.dev@gmail.com>
2022-06-28 22:25:16 -0100

    feat: adding the json file for the models training, and the plot implementation

---
+++ main.py
@@ -0,0 +1,85 @@
+import argparse
+from termcolor import colored
+
+arg_pr = argparse.ArgumentParser()
+
+arg_pr.add_argument(
+    "-m", "--model", nargs="+", required=True,
+    choices=["GRU", "LSTM", "Transformer"],
+    help="Add the model to use on the graphic plot."
+)
+
+arg_pr.add_argument(
+    "-s", "--source", required=True,
+    choices=["en", "cv"],
+    help="Source languague of the model"
+)
+
+arg_pr.add_argument(
+    "-t", "--target", required=True,
+    choices=["en", "cv"],
+    help="Target languague the model"
+)
+
+args = vars(arg_pr.parse_args())
+
+
+if args["source"] == args["target"]:
+    print(
+        colored(
+            "Error: Source languague and Target languague should not be the same.",
+            "red", attrs=["bold"])
+    )
+    exit(1)
+
+
+from src.plot_config import Plot_Metrics_Graphs as plt_MG
+
+
+valid_metrics_name = [
+    "Training Loss", "Validation Loss", "Training Accuracy", "Validation Accuracy"]
+
+
+def validate_metrics(metrics_name: str) -> list:
+    try:
+        metrics_name = metrics_name.split(", ")
+    except:
+        return None
+
+    for metric in metrics_name:
+        if metric not in valid_metrics_name:
+            return None
+    
+    return metrics_name
+
+
+print("\nEnter the metrics name separate by ',' (comma):")
+print(colored(f"Valid list: {valid_metrics_name}\n", "blue", attrs=["bold"]))
+print(colored("Ex: ", attrs=["bold"]))
+print(colored("  Metrics name: Training Loss, Validation Loss", attrs=["bold"]))
+
+metrics_name = str(input(colored("\nMetrics name: ", "magenta", attrs=["bold"])))
+metrics_name = validate_metrics(metrics_name)
+
+if not metrics_name:
+    print(
+        colored(
+            "Error: Matrics name not compatible.",
+            "red", attrs=["bold"])
+    )
+    exit(1)
+
+header_name = str(input(colored("\nPlot header name: ", "magenta", attrs=["bold"])))
+y_label = str(input(colored("\ny_label name: ", "magenta", attrs=["bold"])))
+
+
+
+if __name__ == "__main__":
+    plt_MG.plot_metrics(
+        models = args["model"], 
+        metrics_name = metrics_name, 
+        source = args["source"], 
+        target = args["target"], 
+        plot_header=header_name, 
+        y_label=y_label
+    )
---
+++ src/plot_config.py
@@ -0,0 +1,109 @@
+import json
+import matplotlib.pyplot as plt
+from numpy import pad
+
+colors = ["#3EC70B", "#FF0000", "#3B44F6", "#FF5B00", "#0E185F", "#243D25"]
+
+class Plot_Metrics_Graphs:
+
+    @staticmethod
+    def get_data(model: str, graphs_name: list) -> list:
+        """
+            Method to get the data in one model.
+        """
+        # opening all the json files
+        json_files = [open(f'./{model.lower()}/{graph}.json') for graph in graphs_name]
+
+        # load all the data
+        data = [json.load(file) for file in json_files]
+
+        # close all the files
+        [file.close for file in json_files]
+
+        return data
+    
+    @staticmethod
+    def get_all_data(models: list, graphs_name: list) -> list:
+        """
+            Method to get all the data inside more then one models.
+        """
+        for model in models:
+            data = Plot_Metrics_Graphs.get_data(model, graphs_name)
+            print("\n\n")
+            print(data)
+            print("\n\n")            
+        
+        print()
+        return [Plot_Metrics_Graphs.get_data(model, graphs_name) for model in models]
+    
+    @staticmethod
+    def make_matrics_single_model(graphs_name: list, all_data: list) -> dict:
+        """
+            Method to make a single metrics dict of a model.
+        """
+        metrics = {}
+
+        for graph, data in zip(graphs_name, all_data):
+            metrics[graph] = [[dt[1] for dt in data], [dt[2] for dt in data]]
+        
+        return metrics
+    
+    @staticmethod
+    def make_matrics_multi_models(
+                model: str, simplify_graphs_name: list, all_data: list) -> dict:
+        """
+            Method to make a multiple metrics dict of different models.
+        """
+        metrics = {}
+
+        for graph, data in zip(simplify_graphs_name, all_data):
+            metrics[model+": "+graph] = [[dt[1] for dt in data], [dt[2] for dt in data]]
+        
+        return metrics
+    
+    @staticmethod
+    def make_metrics(models: list, graphs_name: list) -> dict:
+        """
+            Method to make the metrics dict.
+        """
+        all_metrics = {}
+
+        if len(models) > 1:
+            metrics = {}
+            for model in models:
+                all_data = Plot_Metrics_Graphs.get_data(model, graphs_name)
+                metrics = Plot_Metrics_Graphs.make_matrics_multi_models(
+                                model, graphs_name, all_data)
+                all_metrics.update(metrics)
+        
+        else:
+            all_data = Plot_Metrics_Graphs.get_data(models[0], graphs_name)
+            metrics = Plot_Metrics_Graphs.make_matrics_single_model(graphs_name, all_data)
+            all_metrics.update(metrics)
+        
+        return all_metrics
+
+    @staticmethod
+    def plot_metrics(models: list, metrics_name: list,  source: str, \
+            target: str, plot_header: str, y_label: str, x_label="Epochs") -> None:
+        """
+            Method to calculate and plot different models validation accuracy.
+        """
+        print(models)
+        direction = f"{source}-{target}"
+        graphs_name = [f"{name} ({direction})" for name in metrics_name]
+
+        all_metrics = Plot_Metrics_Graphs.make_metrics(models, graphs_name)
+
+        for metric, color in zip(all_metrics.items(), colors[0:len(all_metrics.items())]):
+            plt.plot(metric[1][0], metric[1][1], color=color, label=metric[0])
+        
+        plt.legend()
+        plt.title(f"{plot_header} ({direction})", pad=18,
+            fontname="Times New Roman", fontweight="bold", fontsize=12,
+        )
+        plt.xlabel(x_label, fontname="Times New Roman", fontweight='bold')
+        plt.ylabel(y_label, fontname="Times New Roman", fontweight='bold')
+        plt.grid(axis = "y", linewidth = 0.4)
+        plt.grid(axis = "x", linewidth = 0.2)
+        plt.show()      
